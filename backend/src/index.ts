f539391c72832fc7877abda04cb5caeafb3a7756
alessiogs <alessio.soggiu@hotmail.it>
2025-09-20 17:03:35 +0200

    [frontend] fix: small configurations updated

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,11 @@
 import { Hono } from "hono"
+import { cors } from "hono/cors"
 import me from "./routes/protected/me"
 import auth from "./routes/public/auth"
 
 const app = new Hono()
+
+app.use("*", cors({ origin: ["http://localhost:5173"] }))
 
 app.route("/auth", auth)
 app.route("/me", me)
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,9 +5,9 @@ import "./index.css"
 import AuthProvider from "./context/auth/AuthProvider.tsx"
 
 createRoot(document.getElementById("root")!).render(
-  <AuthProvider>
-    <BrowserRouter>
+  <BrowserRouter>
+    <AuthProvider>
       <App />
-    </BrowserRouter>
-  </AuthProvider>
+    </AuthProvider>
+  </BrowserRouter>
 )
